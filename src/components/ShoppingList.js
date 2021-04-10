3877f9185b3d09484b867daa2bc7ee79ec05d5a4
pj <pjhg14@gmail.com>
2021-04-10 15:35:24 -0400

    commit tree a41b5e7fa671d2f6ff895f507aa8b452594103b9

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,8 +3,9 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, handleAddItem }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("")
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
@@ -16,12 +17,20 @@ function ShoppingList({ items }) {
     return item.category === selectedCategory;
   });
 
+  function handleSearchChange(event) {
+    setSearch(event.target.value)
+  }
+
+  const searchedItems = itemsToDisplay.filter(item => {
+    return item.name.toLowerCase().includes(search.toLowerCase())
+  })
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={handleAddItem}/>
+      <Filter selectedCategory={selectedCategory} onCategoryChange={handleCategoryChange} search={search} onSearchChange={handleSearchChange}/>
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {searchedItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
